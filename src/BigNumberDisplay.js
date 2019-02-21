a80d09cba25c0b2a5156d97902670e5798969cc6
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2019-02-21 19:42:48 +0100

    Update BigNumberDisplay.js

--- src/BigNumberDisplay.js
+++ src/BigNumberDisplay.js
@@ -4,6 +4,17 @@ import Typography from "@material-ui/core/Typography";
 import React, { Component } from "react";
 
 const styles = theme => ({
+  bigNumberPaper: {
+    overflow: "hidden",
+
+    height: "72vw",
+    [theme.breakpoints.up("sm")]: {
+      height: "36vw",
+      [theme.breakpoints.up("xl")]: {
+        height: 720
+      }
+    }
+  },
   bigNumberDisplay: {
     width: "100%",
     textAlign: "center",
@@ -26,13 +37,13 @@ class BigNumberDisplay extends Component {
 
   render() {
     const { classes } = this.props;
-    return (
-      <Paper>
+    return this.props.chosenNumber !== null ? (
+      <Paper className={classes.bigNumberPaper}>
         <Typography className={classes.bigNumberDisplay}>
           {this.props.chosenNumber !== null ? this.props.chosenNumber : ""}
         </Typography>
       </Paper>
-    );
+    ) : null;
   }
 }
 
