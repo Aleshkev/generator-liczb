6183e19919db9a4cff674a33397c96957d5629a6
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2019-02-26 18:31:59 +0100

    Update flask_app.py

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,7 +1,7 @@
 import collections
-import json
 import random
 import itertools
+import string
 import traceback
 
 from flask import Flask, request, abort
@@ -18,14 +18,12 @@ weights.update({
 })
 weighted = tuple(itertools.chain(*([i] * weights[i] for i in range(40))))
 
-
-@app.route('/weighted')
-def get_weighted():
-    return json.dumps(weighted)
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzm0123456789+/"
+compact_weights = ''.join(alphabet[weights[i]] for i in range(40))
 
 
-class CompletelyRandomSequence:
-    def __init__(self, ideal_reservation=128):
+class Sequence:
+    def __init__(self, ideal_reservation=250):
         self.ideal_reservation = ideal_reservation
 
         self.next_numbers = []
@@ -37,6 +35,7 @@ class CompletelyRandomSequence:
             self.next_numbers.append(random.choice(weighted))
 
     def get_next(self):
+        self.i += 1
         self.reserve(self.ideal_reservation)
         x = self.next_numbers[0]
         self.next_numbers = self.next_numbers[1:]
@@ -44,38 +43,61 @@ class CompletelyRandomSequence:
         return x
 
 
-class SequentiallyRandomSequence(CompletelyRandomSequence):
-    def reserve(self, n):
-        l = list(range(40))
-        while len(self.next_numbers) < n:
-            random.shuffle(l)
-            self.next_numbers += l
+sequence = Sequence()
 
+template = string.Template("""
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        font-family: "Source Code Pro", "Courier New", Courier, monospace;
+        padding: 16px;
+        max-width: 346px;
+        margin: auto;
+      }
+      ol {
+        list-style: none;
+        padding: 0;
+        margin-top: 16px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        width: 320px;
+      }
+      li {
+        width: 32px;
+      }
+    </style>
+  </head>
+  <body>
+    Liczby są brane po kolei; jeżeli użytkownik ma zaznaczoną opcję "unikaj
+    powtórzeń" a liczba była wzięta ostatnio, jest pomijana.
 
-completely_random = CompletelyRandomSequence()
-sequentially_random = SequentiallyRandomSequence()
-
-
-print(completely_random.next_numbers, len(completely_random.next_numbers))
-print(sequentially_random.next_numbers, len(sequentially_random.next_numbers))
+    <ol>
+      $items
+    </ol>
+  </body>
+</html>
+""")
 
 
 @app.route('/')
-def hello_world():
-    return (f"completely random: {completely_random.next_numbers}<br/>"
-            f"sequential random: {sequentially_random.next_numbers}")
+def list_sequence():
+    items = ['<li></li>'] * (sequence.i % 10)
+    items.extend(f'<li>{i + 1}</li>' for i in sequence.next_numbers[:len(sequence.next_numbers) - sequence.i % 10])
+    return template.substitute(items='\n'.join(items))
 
 
 @app.route('/get')
 def get():
     try:
-        sequence = {'completely': completely_random, 'sequentially': sequentially_random}[request.args.get('how')]
         mask = request.args.get('whitelist', (1 << 40) - 1, int)
-        assert mask > 0
+        assert 0 < mask < (1 << 40)
         while True:
             x = sequence.get_next()
             if mask & (1 << x):
-                return str(x)
+                return f"{x};{compact_weights}"
     except Exception as e:
         traceback.print_exc()
         return f"<pre>{traceback.format_exc()}</pre>"
